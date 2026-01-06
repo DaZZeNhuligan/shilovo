966979bdefcf72ef6cbb4ff8b4c3cd72865f4f22
Banan <Dazzen108@yandex.ru>
2026-01-06 20:29:47 +0300

    config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+const DB_DSN = 'mysql:host=localhost;dbname=shilovo;charset=utf8mb4';
+const DB_USER = 'root';
+const DB_PASS = 'root'; 
+
+
+
+const BOT_TOKEN = '';
+const CHAT_ID = ''; 
+
+
+
+const HOLD_HOURS = 48;
