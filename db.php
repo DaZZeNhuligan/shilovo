d2a73b2c731a2e8c8a4fafdb35707bf8025f8a49
Banan <Dazzen108@yandex.ru>
2026-01-06 20:29:27 +0300

    db

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+require __DIR__.'/config.php';
+function pdo(): PDO {
+static $pdo; if ($pdo) return $pdo;
+$pdo = new PDO(DB_DSN, DB_USER, DB_PASS, [
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+]);
+return $pdo;
+}
